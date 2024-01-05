6dd5f0d1bb36ee4d07c8bb29983af76db6e25955
rohithreddy <yrohithreddy12@gmail.com>
2024-01-06 01:50:45 +0530

    week 5.1

--- Week 5/counter-react/src/App.jsx
+++ Week 5/counter-react/src/App.jsx
@@ -1,25 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-// let [state, setCount] = {
-//   count: 0 
-// }
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count,setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      title: 'task a',
+      description: 'hello from task a',
+      completed: false,
+    },
+    {
+      title: 'task b',
+      description: 'hello from task b',
+      completed: false,
+    },
+    {
+      title: 'task c',
+      description: 'hello from task c',
+      completed: false,
+    },
+  ]);
 
-  function onClickHandler(){
-    setCount(count+10)
-    console.log(count)
+  function addTodo() {
+    setTodos([...todos, { title: 'new todo title', description: 'new todo description' }]);
+  }
+
+  function DummyButton(){
+    console.log("hi there")
+    return <button>helo</button>
   }
 
   return (
-   <div>
-    <button onClick={onClickHandler}>Number of clicks {count}</button>
-   </div>
-  )
+    <div>
+      <button onClick={addTodo}>Add Todo</button>
+      {todos.map(function (todo, index) {
+        return <Todo key={index} title={todo.title} description={todo.description} />;
+      })}
+      <DarkNewTodos todos={todos}></DarkNewTodos>
+    </div>
+  );
 }
 
-export default App
+function Todo(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <h3>{props.description}</h3>
+    </div>
+  );
+}
+
+function DarkNewTodos(props) {
+  return (
+    <div>
+      {props.todos.map(function (todo) {
+        return <Todo title={todo.title} description={todo.description} />;
+      })}
+      <h2>{props.title}</h2>
+      <h3>{props.description}</h3>
+    </div>
+  );
+}
+
+export default App;
