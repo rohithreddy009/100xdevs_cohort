7fde2e66e5cae1e2c37f003c0e4bd732f311c4e4
rohithreddy <yrohithreddy12@gmail.com>
2024-02-27 20:46:19 +0530

    prisma lecture

---
+++ Week 12/ls/src/index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+export interface Env {
+  DATABASE_URL: string
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const prisma = new PrismaClient({
+      datasourceUrl: env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const response = await prisma.log.create({
+      data: {
+        level: 'Info',
+        message: `${request.method} ${request.url}`,
+        meta: {
+          headers: JSON.stringify(request.headers),
+        },
+      },
+    })
+
+    
+
+    console.log(JSON.stringify(response))
+
+
+	return Response.json(response)
+  },
+}
