69c78039c67f88f1b91313ac7461a6bef0e6165e
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-04-27 21:25:10 +0530

    Week 20

--- Week_20/rate_limiting/src/index.ts
+++ Week_20/rate_limiting/src/index.ts
@@ -25,7 +25,7 @@ app.post('/generate-otp', (req, res) => {
   res.status(200).json({ message: "OTP generated and logged" });
 });
 
-// Endpoint to reset password
+// Endpoint to reset password.
 app.post('/reset-password', (req, res) => {
   const { email, otp, newPassword } = req.body;
   if (!email || !otp || !newPassword) {
@@ -43,3 +43,4 @@ app.post('/reset-password', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
