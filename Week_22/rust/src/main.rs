e2da3da9ae2f7edcc345a0ec7a450b8d37f16db0
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-05-02 22:15:56 +0530

    rust

---
+++ Week_22/rust/src/main.rs
@@ -0,0 +1,26 @@
+// use std::thread;
+
+// fn main() {
+//     // Spawn three threads
+//     for _ in 0..3 {
+//         thread::spawn(|| {
+//             let mut counter: f64 = 0.00;
+//             loop {
+//                 counter += 0.001;
+//             }
+//         });
+//     }
+
+//     loop {
+//         // Main thread does nothing but keep the program alive
+//     }
+// }
+
+
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+}
+
