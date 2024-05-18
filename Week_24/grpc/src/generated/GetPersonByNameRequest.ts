00d4655d86f189a2bb9c60db6dc7195bc41a3567
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-05-18 18:39:17 +0530

    grpc,jsonrpc,trpc

---
+++ Week_24/grpc/src/generated/GetPersonByNameRequest.ts
@@ -0,0 +1,10 @@
+// Original file: src/index.proto
+
+
+export interface GetPersonByNameRequest {
+  'name'?: (string);
+}
+
+export interface GetPersonByNameRequest__Output {
+  'name': (string);
+}
---
+++ Week_24/grpc/src/generated/Person.ts
@@ -0,0 +1,12 @@
+// Original file: src/index.proto
+
+
+export interface Person {
+  'name'?: (string);
+  'age'?: (number);
+}
+
+export interface Person__Output {
+  'name': (string);
+  'age': (number);
+}
